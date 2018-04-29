bbd423c6df44b25942e371963ae5162e53d49b2d
Shripad Gaikwad <shripad.gaikwad@live.com>
2018-04-29 22:21:04 +0530

    added HTTP mocking

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,15 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname,'../src/index.html'));
 });
 
+app.get('/users', function(req, res) {
+  // Hard coded for simplicity
+  res.json([
+    {"id": 1, "firstName": "Shruti", "lastName": "Gaikwad", "email": "shruti@gmail.com"},
+    {"id": 2, "firstName": "Shruti1", "lastName": "Gaikwad", "email": "shruti2@gmail.com"},
+    {"id": 3, "firstName": "Shruti2", "lastName": "Gaikwad", "email": "shruti3@gmail.com"}
+  ]);
+});
+
 app.listen(port, function(err) {
   if (err) {
     console.log(err);
---
+++ src/api/userApi.js
@@ -0,0 +1,32 @@
+import 'whatwg-fetch';
+import getBaseUrl from './baseUri';
+
+const baseUrl = getBaseUrl();
+
+export function getUsers() {
+  return get('users');
+}
+
+export function deleteUser(id) {
+  return del(`users/${id}`);
+}
+
+function get(url) {
+  return fetch(baseUrl + url).then(onSucess, onError);
+}
+
+function del(url) {
+  const request = new Request(baseUrl + url, {
+    method: 'DELETE'
+  });
+
+  return fetch(request).then(onSucess, onError);
+}
+
+function onSucess(response) {
+  return response.json();
+}
+
+function onError(error) {
+  console.log(error) // eslint-disable-line no-console
+}
